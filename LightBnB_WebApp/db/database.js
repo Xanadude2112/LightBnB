42aec554f7d8203610d5a027ba0a440880e75bab
Thomas Azran <tommy.azran@gmail.com>
2024-04-14 23:58:42 +0000

    implement more tables to show properties on reservations

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -99,11 +99,14 @@ const addUser = function (user) {
 const getAllReservations = function (guest_id, limit = 10) {
   return pool
   .query(`
-    SELECT reservations.*
-    FROM reservations
-    JOIN users ON reservations.guest_id = users.id
-    WHERE reservations.guest_id = $1
-    LIMIT $2
+   SELECT properties.*, reservations.*, AVG(property_reviews.rating) AS average_rating
+   FROM reservations
+   JOIN properties ON reservations.property_id = properties.id
+   JOIN property_reviews ON properties.id = property_reviews.property_id
+   WHERE reservations.guest_id = $1
+   GROUP BY properties.id, reservations.id
+   ORDER BY reservations.start_date
+   LIMIT $2
   `,
   [guest_id, limit])
   .then((result) => {
